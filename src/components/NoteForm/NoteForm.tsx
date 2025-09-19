88bfa38c12f62aaa846bc7a5a47775d2487e5dbc
Alexey <makovii88@gmail.com>
2025-09-19 20:00:42 +0200

    update YUP

--- src/components/NoteForm/NoteForm.tsx
+++ src/components/NoteForm/NoteForm.tsx
@@ -57,77 +57,79 @@ const NoteForm = ({ onClose }: NoteFormProps) => {
       onSubmit={handleSubmit}
       validationSchema={NoteFormSchema}
     >
-      <Form className={css.form}>
-        <div className={css.formGroup}>
-          <label htmlFor="title">Title</label>
-          <Field
-            id="title"
-            type="text"
-            name="title"
-            className={css.input}
-          />
-          <ErrorMessage
-            name="title"
-            component="span"
-            className={css.error}
-          />
-        </div>
+      {({ isValid, dirty, isSubmitting }) => (
+        <Form className={css.form}>
+          <div className={css.formGroup}>
+            <label htmlFor="title">Title</label>
+            <Field
+              id="title"
+              type="text"
+              name="title"
+              className={css.input}
+            />
+            <ErrorMessage
+              name="title"
+              component="span"
+              className={css.error}
+            />
+          </div>
 
-        <div className={css.formGroup}>
-          <label htmlFor="content">Content</label>
-          <Field
-            as="textarea"
-            id="content"
-            name="content"
-            rows={8}
-            className={css.textarea}
-          />
-          <ErrorMessage
-            name="content"
-            component="span"
-            className={css.error}
-          />
-        </div>
+          <div className={css.formGroup}>
+            <label htmlFor="content">Content</label>
+            <Field
+              as="textarea"
+              id="content"
+              name="content"
+              rows={8}
+              className={css.textarea}
+            />
+            <ErrorMessage
+              name="content"
+              component="span"
+              className={css.error}
+            />
+          </div>
 
-        <div className={css.formGroup}>
-          <label htmlFor="tag">Tag</label>
-          <Field
-            as="select"
-            id="tag"
-            name="tag"
-            className={css.select}
-          >
-            <option value="Todo">Todo</option>
-            <option value="Work">Work</option>
-            <option value="Personal">Personal</option>
-            <option value="Meeting">Meeting</option>
-            <option value="Shopping">Shopping</option>
-          </Field>
+          <div className={css.formGroup}>
+            <label htmlFor="tag">Tag</label>
+            <Field
+              as="select"
+              id="tag"
+              name="tag"
+              className={css.select}
+            >
+              <option value="Todo">Todo</option>
+              <option value="Work">Work</option>
+              <option value="Personal">Personal</option>
+              <option value="Meeting">Meeting</option>
+              <option value="Shopping">Shopping</option>
+            </Field>
 
-          <ErrorMessage
-            name="tag"
-            component="span"
-            className={css.error}
-          />
-        </div>
+            <ErrorMessage
+              name="tag"
+              component="span"
+              className={css.error}
+            />
+          </div>
 
-        <div className={css.actions}>
-          <button
-            type="button"
-            className={css.cancelButton}
-            onClick={onClose}
-          >
-            Cancel
-          </button>
-          <button
-            type="submit"
-            className={css.submitButton}
-            //   disabled=false
-          >
-            Create note
-          </button>
-        </div>
-      </Form>
+          <div className={css.actions}>
+            <button
+              type="button"
+              className={css.cancelButton}
+              onClick={onClose}
+            >
+              Cancel
+            </button>
+            <button
+              type="submit"
+              className={css.submitButton}
+              disabled={!isValid || !dirty || isSubmitting}
+            >
+              Create note
+            </button>
+          </div>
+        </Form>
+      )}
     </Formik>
   );
 };
